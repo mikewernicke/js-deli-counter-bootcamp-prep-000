498bb8204b15e1b526d47f4dc7ae6536c50ce2af
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-22 16:07:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = [];
+
+function takeANumber (katzDeli, name) {
+  katzDeli = katzDeli.push[name];
+  var n = katzDeli.length
+  return ("Welcome ", name, ". You are number ", n + 1," in line.");
+}
+function nowServing() {
+//  if katzDeliLine = {0:"Empty"} {
+//    return("There is nobody waiting to be served!")
+//  }
+//  else {
+//    return ("Currently serving ${katzDeliLine.1}");
+//    delete katzDeliLine.1;
+//  }
+//  }
+}
+function currentLine() {
+//  if (katzDeliLine.length <1) {
+//    return ("The line is currently" ${katzDeliLine.0});
+//  }
+//  else if (array.length > 0) {
+//    for (var i = 0, i < array.length, ++i) {
+//      return ()
+//    }
+//  }
+//  }
+}
